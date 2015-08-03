24073e8ee1c5bd17c491996dfcfa5112a49fe85e
Panashe Fundira <fundirap@gmail.com>
2015-08-02 20:39:16 -0400

    Change brace style to Allman
    Allman seems to promote more modularity to me (since you can comment out the first line of a block and still works) and seems intuitive that the braces of a block should line up. Now that I have a full HD monitor, vertical space isn't at the premium that it once was either.

--- Data_Structures/Array_String/UniqueString.java
+++ Data_Structures/Array_String/UniqueString.java
@@ -1,21 +1,29 @@
 import java.util.HashSet;
 
-public class UniqueString {
- public static void main(String[] args) {
+public class UniqueString
+{
+ public static void main(String[] args)
+ {
   System.out.println(solve(args[0]));
  } 
 
- public static boolean solve(String str) {
+ public static boolean solve(String str)
+ {
    if (str.length() > 128)
+   {
      return false;
+   }
 
    HashSet<Character> seen = new HashSet<Character>();
-   for (int i = 0; i < str.length(); i++) {
+   for (int i = 0; i < str.length(); i++)
+   {
      char c = str.charAt(i);
-     if (seen.contains(c)) {
+     if (seen.contains(c))
+     {
        return false;
      }
-     else {
+     else
+     {
        seen.add(c);
      }
    }
