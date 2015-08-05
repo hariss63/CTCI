7a423df4f1c15f37a8d09d9abc9c581ca05647a2
Panashe Fundira <fundirap@gmail.com>
2015-08-05 11:34:47 -0400

    Change indentation from tabs to spaces

--- Data_Structures/Array_String/StringCompression.java
+++ Data_Structures/Array_String/StringCompression.java
@@ -1,50 +1,50 @@
 public class StringCompression
 {
-	public static void main(String[] args)
-	{
-		String str = "aabccccaaa";
-		solve(str);		
-	}
+  public static void main(String[] args)
+  {
+    String str = "aabccccaaa";
+    solve(str);		
+  }
  
-	public static String solve(String str)
-	{
-		if (newLength(str) >= str.length())
-		{
-			return str;
-		}
-		StringBuilder sb = new StringBuilder();
-		int streak = 1;
-		for (int i = 1; i < str.length(); i++)
-		{
-			if (str.charAt(i) == str.charAt(i-1))
-			{
-				streak++;
-			}
-			else
-			{
-				sb.append(str.charAt(i-1));
-				sb.append(streak);
-				streak = 1;
-			}
-		}
-		sb.append(str.charAt(str.length() - 1));
-		sb.append(streak);
-		return sb.toString();
-	}
-	private static int newLength(String str)
-	{
-		if (str.length() <= 2)
-		{
-			return 2;
-		}
-		int streakCount = 1;
-		for (int i = 1; i < str.length(); i++)
-		{
-			if (str.charAt(i) != str.charAt(i-1))
-			{
-				streakCount++;
-			}
-		}
-		return 2 * streakCount;
-	}
+  public static String solve(String str)
+  {
+    if (newLength(str) >= str.length())
+    {
+      return str;
+    }
+    StringBuilder sb = new StringBuilder();
+    int streak = 1;
+    for (int i = 1; i < str.length(); i++)
+    {
+      if (str.charAt(i) == str.charAt(i-1))
+      {
+        streak++;
+      }
+      else
+      {
+        sb.append(str.charAt(i-1));
+        sb.append(streak);
+        streak = 1;
+      }
+    }
+    sb.append(str.charAt(str.length() - 1));
+    sb.append(streak);
+    return sb.toString();
+  }
+  private static int newLength(String str)
+  {
+    if (str.length() <= 2)
+    {
+      return 2;
+    }
+    int streakCount = 1;
+    for (int i = 1; i < str.length(); i++)
+    {
+      if (str.charAt(i) != str.charAt(i-1))
+      {
+        streakCount++;
+      }
+    }
+    return 2 * streakCount;
+  }
 }
