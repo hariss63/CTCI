eb6147d9e2e4d655df8dcc5be7daa7a55fd3f18f
Panashe M. Fundira <fundirap@gmail.com>
2015-08-10 21:46:22 -0400

    Update README

---
+++ Data_Structures/Linked_List/Partition.java
@@ -0,0 +1,62 @@
+import java.util.HashSet;
+
+public class Partition
+{
+  private static class Node
+  {
+    Node next = null;
+    int data;
+
+    public Node(int d)
+    {
+      data = d;
+    }
+
+    void add(int d)
+    {
+      Node end = new Node(d);
+      Node n = this;
+      while (n.next != null)
+      {
+        n = n.next;
+      }
+      n.next = end;
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    Node linked = new Node(5);
+    linked.add(2);
+    linked.add(3);
+    linked.add(2);
+    linked.add(7);
+    printList(linked);
+    Node del = linked.next.next;
+    deleteNode(del);
+    printList(linked);
+  }
+
+
+  private static void printList(Node head)
+  {
+    Node n = head;
+    while (n.next != null)
+    {
+      System.out.printf("%d -> ", n.data);
+      n = n.next;
+    }
+    System.out.println(n.data);
+  }
+
+  private static void deleteNode(Node head, int val)
+  {
+    if (head == null || head.next == null) {
+      head = null;
+    }
+    else {
+      head.data = head.next.data;
+      head.next = head.next.next;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -33,3 +33,5 @@ FOLLOW UP
 How would you solve this problem if a temporary buffer is not allowed?
 
 **[2.2 Return Kth to Last:](https://github.com/munyari/CTCI/blob/master/Data_Structures/Linked_List/KthToLast.java)** Implement an algorithm to find the kth to last element of a singly linked list.
+
+**[2.3 Delete Middle Node:](https://github.com/munyari/CTCI/blob/master/Data_Structures/Linked_List/DeleteMiddleNode.java)** Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
